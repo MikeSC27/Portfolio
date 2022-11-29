3b3a859b5e73c5dfdd1ce061b8ecda30dd8ac33c
MikeSC27 <mcruzen89@gmail.com>
2022-11-29 08:54:45 -0800

    need to update game projects and figure out contact input to be sent to my email for review and footer personal contact section needs transition to left side only.

---
+++ portfolio-santacruz/src/components/HeroImg.js
@@ -0,0 +1,24 @@
+import "./HeroImgStyles.css";
+import React from 'react'
+import IntroImg from "../assets/pfbkgrndimg.jpeg"
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+            <img className="intro-img" src={IntroImg} alt="IntroImg" />
+        </div>
+        <div className="content">
+            <p>FULL STACK</p>
+            <h1>React and Ruby on Rails Developer</h1>
+            <div>
+                <Link to="/project" className="btn">Projects</Link>
+                <Link to="/contact" className="btn-light">Contact</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroImg
---
+++ portfolio-santacruz/src/components/WorkCard.js
@@ -0,0 +1,25 @@
+import "./WorkCardStyles.css"
+import React from 'react'
+import { NavLink } from "react-router-dom"
+
+const WorkCard = (props) => {
+  return (
+    <div className="project-card">
+    <img src={props.imgsrc} alt="image" />
+    <h2 className="project-title">{props.title}</h2>
+    <div className="pro-details">
+        <p>{props.text}</p>
+        <div className="pro-btns">
+            <NavLink to={props.view} className="btn">
+                Link to Game
+            </NavLink>
+            {/* <NavLink to="puturladdyhere.com" className="btn">
+                Link to GITHUB
+            </NavLink> */}
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default WorkCard
